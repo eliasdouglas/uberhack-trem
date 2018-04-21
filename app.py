fdf79337963cd945f01d7d85978c2d755e89291c
Douglas Rodrigues <douglas.rodrigues@eteg.com.br>
2018-04-21 17:31:21 -0300

    Estruturando projeto

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+class App(Flask):
+    """comentario"""
+
+    def __init__(self, name):
+        super(App, self).__init__(name)
+
+        # Inicializa as rotas
+        self.add_url_rule('/',
+                          methods=['GET'],
+                          view_func=self.__hello)
+
+        self.add_url_rule('/trace-routes',
+                          methods=['POST'],
+                          view_func=self.__trace_routes)
+
+    def __hello(self):
+        """testa se o servidor está em execução"""
+        return "Server is alive!"
+
+    def __trace_routes(self):
+        """traça rotas de acordo com a origem de destino passado por parâmetro"""
+        routes = []
+
+        # TODO obter rotas do google api
+        # TODO obter rota da uber api
+
+        return routes
+
+
+if __name__ == "__main__":
+    App(__name__).run('0.0.0.0', port=5000)
